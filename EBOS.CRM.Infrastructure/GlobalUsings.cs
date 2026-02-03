f9488bf3841aad30ed342045e9d4e7a4cfd8339f
jpardogarcia7310 <jmpardogarcia7310@icloud.com>
2026-02-03 17:59:25 +0100

    Global usings

---
+++ EBOS.CRM.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+global using EBOS.CRM.Infrastructure.Persistence;
